Fixed incorrect calculations in LBDM. Refactoring + Visualisation

project/util/lbdm.py
@@ -1,17 +1,23 @@
 import mido
-
 from typing import List, Callable
+import numpy as np
+
+from .midtools import get_note_timeline
 
 
-def degree_of_change(x1: int, x2: int) -> float:
+def default_change(x1: int, x2: int) -> float:
     if x1 == 0 and x2 == 0:
         return 0
     else:
         return abs(x1 - x2) / (x1 + x2)
 
 
-def lbdm(track: mido.MidiTrack, pitch_weight: float = 0.25, ioi_weight: float = 0.5, rest_weight: float = 0.25 \
-        ,change_func : Callable[[int,int],float] = degree_of_change) -> List[float]:
+def normalize(arr: np.array) -> np.array:
+    return arr / np.max(arr)
+
+
+def lbdm(track: mido.MidiTrack, pitch_weight: float = 0.25, ioi_weight: float = 0.5, rest_weight: float = 0.25
+         , degree_of_change: Callable[[int, int], float] = default_change) -> List[float]:
     """
     Creates a list of boundaries for the target MIDI track using the Local Boundary Detection Model.
     It looks at the intervals between notes in terms of pitch, inter-onsets (between the starts of notes pressing down)
@@ -23,38 +29,67 @@ def lbdm(track: mido.MidiTrack, pitch_weight: float = 0.25, ioi_weight: float = 
         pitch_weight: The relative importance of pitches in determining where boundaries are placed
         ioi_weight: The relative importance of inter-onset intervals in determining where boundaries are placed
         rest_weight: The relative importance of rests in determining where boundaries are placed
-        change_func: A function to calculate the relative difference between two intervals
+        degree_of_change: A function to calculate the relative difference between two intervals
     Returns: A boundary strength profile describing the places in which the music changes
 
     """
     if len(track) < 2:
         return []
+
     # get only note_on / note_off events
-    note_msgs = []
+    notes = get_note_timeline(track)
+
     pitches = []
     interonsets = []
     rests = []
 
-    # notes: (note, start, end)
+    # create parametric profile
+    for i in range(1, len(notes)):
+        pitches.append(abs(notes[i][1] - notes[i - 1][1]))
+        interonsets.append(notes[i][0] - notes[i - 1][0])
+        rests.append(0)  # rest is temp set to 0
 
-    for i in range(len(track)):
-        if track[i].type == "note_on" and track[i].velocity != 0:  # ignore note off / running status note off messages
-            note_msgs.append(track[i])
+    sequence_pitches = []
+    sequence_iois = []
+    sequence_rests = []
 
-    for j in range(1, len(note_msgs)):
-        pitches.append(abs(note_msgs[j].note - note_msgs[j - 1].note))
-        interonsets.append(abs(note_msgs[j].time - note_msgs[j - 1].time))
-        rests.append(0)  # temp
+    # determine the change between intervals
+    for i in range(len(pitches)):
+        prev_pitch = 0
+        prev_ioi = 0
+        prev_rest = 0
 
-    print(pitches)
-    print(interonsets)
-    print(rests)
+        next_pitch = 0
+        next_ioi = 0
+        next_rest = 0
 
-    pitch_sequences = []
-    ioi_sequences = []
-    rest_sequences = []
+        if i != 0:
+            prev_pitch = pitches[i - 1]
+            prev_ioi = interonsets[i - 1]
+            prev_rest = rests[i - 1]
+        if i < len(pitches) - 1:
+            next_pitch = pitches[i + 1]
+            next_ioi = interonsets[i + 1]
+            next_rest = rests[i + 1]
 
+        sequence_pitches.append((pitches[i] *
+                                (degree_of_change(prev_pitch, pitches[i])
+                                + (degree_of_change(pitches[i], next_pitch)))) + 1)
 
+        sequence_iois.append((interonsets[i] *
+                             (degree_of_change(prev_ioi, interonsets[i])
+                             + (degree_of_change(interonsets[i], next_ioi)))) + 1)
 
+        sequence_rests.append((rests[i] *
+                              (degree_of_change(prev_rest, rests[i])
+                              + (degree_of_change(rests[i], next_rest)))) + 1)
+
+    # normalise to range [0,1]
+    sequence_pitches = normalize(np.array(sequence_pitches))
+    sequence_iois = normalize(np.array(sequence_iois))
+    sequence_rests = normalize(np.array(sequence_rests))
+
+    sequence_profile = (sequence_pitches * pitch_weight) + (sequence_iois * ioi_weight) + (sequence_rests * rest_weight)
+    return list(sequence_profile)
 
 