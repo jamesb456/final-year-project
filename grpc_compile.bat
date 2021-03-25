505706a3da7b43ca5a3d3623e7b067a6180d3c14
James Bray <jxb1123@student.bham.ac.uk>
2021-03-25 23:32:09 +0000

    Update gRPC compile command

--- grpc_compile.bat
+++ grpc_compile.bat
@@ -1,1 +1,1 @@
-python -m grpc_tools.protoc -Iproject/server/protos  --python_out=project/server --grpc_python_out=project/server  project/server/protos/query_server.proto
+python -m grpc_tools.protoc -Iproject/server/protos  --python_out=. --grpc_python_out=. project/server/protos/project/server/query_handler.proto
